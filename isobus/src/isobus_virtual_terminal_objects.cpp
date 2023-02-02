30fc3fba06b266b1f9d085fa723180ae2dd3bc35
Tom Stirnkorb <tom@stirnkorb.me>
2023-02-02 21:31:09 +0100

    introduce read_bytes_at function

--- isobus/src/isobus_virtual_terminal_objects.cpp
+++ isobus/src/isobus_virtual_terminal_objects.cpp
@@ -7,11 +7,25 @@
 /// @copyright 2023 Adrian Del Grosso
 //================================================================================================
 #include "isobus/isobus/isobus_virtual_terminal_objects.hpp"
-#include "isobus/isobus/can_warning_logger.hpp"
+#include "isobus/isobus/can_stack_logger.hpp"
 #include "isobus/utility/to_string.hpp"
 #include "isobus/utility/iop_file_interface.hpp"
 
-#include <iostream>
+/// @brief Converts bytes from a vector at a given iterator to a value of type T
+/// @tparam T Type to convert to
+/// @param it Iterator of the start of the bytes to convert
+/// @returns The converted value
+template <typename T>
+T read_bytes_at(std::vector<std::uint8_t>::iterator &it)
+{
+    T value = *it++;
+    for (auto i = 1; i < sizeof(T); ++i)
+    {
+        T tmp = *it++ << (i * 8);
+        value |= tmp;
+    }
+    return value;
+}
 
 namespace isobus
 {
@@ -25,7 +39,7 @@ namespace isobus
         return this->objectID;
     }
 
-    void VirtualTerminalObject::register_update_callback(const VTObjectChangedCallback callback)
+    void VirtualTerminalObject::register_update_callback(const VTObjectChangedCallback& callback)
     {
         this->objectChangedCallbacks.push_back(callback);
     }
@@ -45,26 +59,26 @@ namespace isobus
 
     bool WorkingSetObject::parse(const std::vector<std::uint8_t> &bytes, std::vector<std::uint8_t>::iterator &it)
     {
-        this->objectID = static_cast<ObjectID>(*it++ | *it++ << 8);
+        this->objectID = read_bytes_at<ObjectID>(it);
         it++; // skip object type
         this->backgroundColor = *it++;
         this->selectable = static_cast<bool>(*it++);
-        this->activeMask = static_cast<ObjectID>(*it++ | *it++ << 8);
+        this->activeMask = read_bytes_at<ObjectID>(it);
         std::uint8_t numObjects = *it++;
         std::uint8_t numMacros = *it++;
         std::uint8_t numLanguages = *it++;
 
         if (bytes.end() - it < numObjects * 6 + numMacros * 2 + numLanguages * 2)
         {
-            CANStackLogger::CAN_stack_log("[Object Pool Parser] Failed to parse working set object: invalid pool size!");
+            CANStackLogger::CAN_stack_log(CANStackLogger::LoggingLevel::Error, "[Object Pool Parser] Failed to parse working set object: invalid pool size!");
             return false;
         }
 
         for (int i = 0; i < numObjects; ++i)
         {
-            ObjectID id = *it++ | *it++ << 8;
-            std::int16_t x_location = *it++ | *it++ << 8;
-            std::int16_t y_location = *it++ | *it++ << 8;
+            ObjectID id = read_bytes_at<ObjectID>(it);
+            auto x_location = read_bytes_at<std::int16_t>(it);
+            auto y_location = read_bytes_at<std::int16_t>(it);
             childObjects.push_back(std::make_tuple(id, x_location, y_location));
         }
 
@@ -115,7 +129,6 @@ namespace isobus
         auto iterator = binaryPool.begin();
         while (iterator != binaryPool.end())
         {
-            ObjectID id = iterator[0] | iterator[1] << 8;
             auto objecttype = static_cast<ObjectType>(iterator[2]);
             switch (objecttype)
             {
@@ -124,12 +137,12 @@ namespace isobus
                 auto workingsetObject = std::make_shared<WorkingSetObject>();
                 if (!workingsetObject->parse(binaryPool, iterator))
                     return false;
-                objects[id] = static_cast<std::shared_ptr<VirtualTerminalObject>>(workingsetObject);
+                objects[workingsetObject->get_object_id()] = static_cast<std::shared_ptr<VirtualTerminalObject>>(workingsetObject);
                 break;
             }
             default:
             {
-                CANStackLogger::CAN_stack_log("[Object Pool Parser] Invalid or unsupported object pool element detected: " + isobus::to_string(static_cast<uint8_t>(objecttype)));
+                CANStackLogger::CAN_stack_log(CANStackLogger::LoggingLevel::Error, "[Object Pool Parser] Invalid or unsupported object pool element detected: " + isobus::to_string(static_cast<uint8_t>(objecttype)));
                 return false;
             }
             }
